ccc8c6e9b983cb5bcf7ad3d2ea29bce7fff275f5
sy97 <shienyuan97@gmail.com>
2020-12-14 14:50:56 +1300

    NEW ARTICLE

---
+++ post/top-5-warframe-primary-weapons-in-2020.md
@@ -0,0 +1,46 @@
+---
+title: Top 5 Warframe primary weapons in 2020
+created_at: 2020-12-14T00:00:00.000Z
+read_min: 5 min
+excerpt: According to the Warframe wiki, there's currently 257 primary weapons in-game, it's hard for players to decide which weapon is good to use and craft and most importantly which weapon worth the time to use Forma(add, remove, alter weapon mod slot polarity) and installing an Orokin Catalyst(increase weapon mod capacity).
+image: https://ik.imagekit.io/seaw0jfghdk/guide/20180714100731_1_aEwvKnOsA.jpg
+image_caption: warframe-primary-weapons
+tags: tag1, tag2
+keywords: warframe primary weapon, ignis wraith, acceltra, rubico prime, kuva bramma, kuva kohm
+---
+
+There's no such weapon as the best weapon in Warframe it all comes down to your personal preference. The strength and use of your mods also heavily affect weapon performance. The hardest part is when it comes to Forma which takes a lot of time to level up the weapon and alter it's mod polarities and Orokin Catalyst is never an easy item to obtain, but if you decided to use Platinums then there are no such hassles.
+
+Enough of introduction, let's get down to the list of primary weapons that I think is great and good to have. Be aware that the following weapons I recommend are heavily opinionated, you should always choose the weapon that fits your playstyle. Sorry if your favourite weapon is not on the list.
+
+---
+
+import weapon from '~/components/guide/WeaponCard.vue'
+import kuvaKohm from '../data/weapon/kuvaKohm.json'
+import kuvaBramma from '../data/weapon/kuvaBramma.json'
+import rubicoPrime from '../data/weapon/rubicoPrime.json'
+import acceltra from '../data/weapon/acceltra.json'
+import ignisWraith from '../data/weapon/ignisWraith.json'
+
+### 5. Kuva Kohm
+
+<weapon class="mb-5 mt-3" :data="kuvaKohm"></weapon>
+
+### 4. Kuva Bramma
+
+<weapon class="mb-5 mt-3" :data="kuvaBramma"></weapon>
+
+### 3. Rubico Prime
+
+<weapon class="mb-5 mt-3" :data="rubicoPrime"></weapon>
+
+### 2. Acceltra
+
+<weapon class="mb-5 mt-3" :data="acceltra"></weapon>
+
+### 1. Ignis Wraith
+
+<weapon class="mb-5 mt-3" :data="ignisWraith"></weapon>
+
+That's the end of the list, remember none of these weapons is a must-have, it all comes to what you prefer 
+to fight your way through the galaxy.
---
+++ src/components/guide/WeaponCard.vue
@@ -0,0 +1,94 @@
+<template>
+    <div>
+        <q :cite="data.link">{{ data.intro }}</q
+        ><span class="text-secondary"> -- Warframe Wiki</span>
+
+        <b-card class="mt-3" no-body text-variant="dark" :header="data.name">
+            <b-list-group flush>
+                <b-list-group-item class="text-center" variant="dark">
+                    <b-img
+                        height="150"
+                        :src="data.img"
+                        :alt="`warframe-${data.name}`"
+                    />
+                </b-list-group-item>
+                <b-list-group-item
+                    class="d-flex justify-content-between align-items-center"
+                >
+                    <span>Mastery</span>
+                    <span>{{ data.mastery }}</span>
+                </b-list-group-item>
+                <b-list-group-item
+                    class="d-flex justify-content-between align-items-center"
+                >
+                    <span>Type</span>
+                    <span>{{ data.type }}</span>
+                </b-list-group-item>
+                <b-list-group-item
+                    class="d-flex justify-content-between align-items-center"
+                >
+                    <span>Trigger</span>
+                    <span>{{ data.trigger }}</span>
+                </b-list-group-item>
+                <b-list-group-item
+                    class="d-flex justify-content-between align-items-center"
+                >
+                    <span>Acquisition</span>
+                    <span>{{ data.acquisition }} </span>
+                </b-list-group-item>
+                <b-list-group-item
+                    class="d-flex justify-content-between align-items-center"
+                >
+                    <span>Acquire Difficulty</span>
+                    <span>
+                        <b-form-rating
+                            class="p-0"
+                            :value="data.acLevel"
+                            no-border
+                            readonly
+                        ></b-form-rating>
+                    </span>
+                </b-list-group-item>
+
+                <b-list-group-item>
+                    <b-embed
+                        :src="data.video"
+                        allowfullscreen
+                        type="iframe"
+                        aspect="16by9"
+                    >
+                    </b-embed>
+                </b-list-group-item>
+            </b-list-group>
+            <b-card-footer
+                ><b-button
+                    block
+                    target="_blank"
+                    variant="primary"
+                    :href="data.link"
+                    >More Details</b-button
+                ></b-card-footer
+            >
+        </b-card>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'WeaponCard',
+    props: {
+        data: Object
+    }
+    // props: {
+    //     name: String,
+    //     img: String,
+    //     intro: String,
+    //     mastery: String,
+    //     type: String,
+    //     trigger: String,
+    //     link: String
+    // }
+};
+</script>
+
+<style scoped></style>
